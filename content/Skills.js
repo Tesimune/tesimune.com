d471a64981adebe1510ed8a1b7a8e5365f1edfb6
Teslim Tesimune <tesimune@gmail.com>
2022-04-04 22:25:53 -0700

    second commit

--- content/Skills.js
+++ content/Skills.js
@@ -5,63 +5,63 @@ export default function Skills() {
   return (
     <div className='bg-gray-200 text-center p-12 shadow-slate-400' id='skills'>
         <button className='border-2 py-1 px-9 mt-6 mb-10 border-black hover:bg-slate-900 hover:text-white'>SKILLS</button>
-        <p className='flex md:gap-20 justify-center mb-10'>
+        <div className='flex md:gap-20 justify-center mb-10'>
             <span className='bg-transparent-500 md:p-3 w-28 h-36'>
                 <Image className='rounded-full' src='/html-5.png' alt='html' width={50} height={50} />
                 <p className='font-bold m-3'>HTML</p>
-                <p></p>
+                
             </span>
             <span className='bg-transparent-500 md:p-3 w-28 h-36'>
                 <Image className='rounded-full' src='/css.png' alt='html' width={50} height={50} />
                 <p className='font-bold m-3'>CSS</p>
-                <p></p>
+                
             </span>
             <span className='bg-transparent-500 ,d:p-3 w-28 h-36'>
                 <Image className='rounded-full' src='/js.png' alt='html' width={50} height={50} />
                 <p className='font-bold m-3'>JavaScript</p>
-                <p></p>
+                
             </span>
             <span className='bg-transparent-500 md:p-3 w-28 h-36'>
                 <Image className='rounded-full' src='/php (1).png' alt='html' width={50} height={50} />
                 <p className='font-bold m-3'>PHP</p>
-                <p></p>
+                
             </span>
             <span className='bg-transparent-500 md:p-3 w-28 h-36'>
                 <Image className='rounded-full' src='/mysql.png' alt='html' width={50} height={50} />
                 <p className='font-bold m-3'>MySQL</p>
-                <p></p>
+                
             </span>
-        </p>
+        </div>
 
        
 
-        <p className='flex md:gap-20 justify-center mb-10'>
+        <div className='flex md:gap-20 justify-center mb-10'>
             <span className='bg-transparent-500 md:p-3 w-28 h-36'>
                 <Image className='rounded-full' src='/figma.png' alt='html' width={50} height={50} />
                 <p className='font-bold m-3'>Figma</p>
-                <p></p>
+                
             </span>
             <span className='bg-transparent-500 md:p-3 w-28 h-36'>
                 <Image className='rounded-full' src='/bootstrap.png' alt='html' width={50} height={50} />
                 <p className='font-bold m-3'>Bootstrap</p>
-                <p></p>
+               
             </span>
             <span className='bg-transparent-500 md:p-3 w-28 h-36'>
                 <Image className='rounded-full' src='/react.png' alt='html' width={50} height={50} />
                 <p className='font-bold m-3'>React</p>
-                <p></p>
+               
             </span>
             <span className='bg-transparent-500 md:p-3 w-28 h-36'>
                 <Image className='rounded-full' src='/php.png' alt='html' width={50} height={50} />
                 <p className='font-bold m-3'>Laravel</p>
-                <p></p>
+                
             </span>
             <span className='bg-transparent-500 md:p-3 w-28 h-36'>
                 <Image className='rounded-full' src='/github.png' alt='html' width={50} height={50} />
                 <p className='font-bold m-3'>GitHub</p>
-                <p></p>
+               
             </span>
-        </p>
+        </div>
     </div>
   )
 }
--- content/components/Footer.js
+++ content/components/Footer.js
@@ -1,22 +1,28 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import {BsArrowUpSquareFill} from 'react-icons/bs'
+import {TiSocialFacebook} from 'react-icons/ti'
+import {TiSocialInstagram} from 'react-icons/ti'
+import {TiSocialTwitter} from 'react-icons/ti'
+import {TiSocialLinkedin} from 'react-icons/ti'
+import {SiWhatsapp} from 'react-icons/si'
 
 
 export default function Footer() {
   return (
     <footer className='grid justify-center bg-slate-900 text-white text-center py-9'>
-      <a href="#" className='mb-3'>BACK TO THE TOP</a>
-      <span className='flex gap-3 mb-3'>
-        <a href='https//:facebook.com/tesimune'>facebook</a>
-        <a href='https//:instagram.com/tesimune'>instagram</a>
-        <a href='https//:twitter.com/tesimune'>twitter</a>
-        <a href='https//:linkedin.com/tesimune'>linkedin</a>
-        <a href='https//:wa.com/tesimune'>whatsapp</a>
+      <a href="#" className='flex gap-3 justify-center mb-3'>
+      <span>BACK TO THE TOP</span>
+      <span><BsArrowUpSquareFill className='w-7 h-6'/></span>
+      </a>
+      <span className='flex justify-center gap-3 mb-3'>
+        <a href='https//:facebook.com/tesimune' target='_blank'><TiSocialFacebook/></a>
+        <a href='https//:instagram.com/tesimune' target='_blank'><TiSocialInstagram/></a>
+        <a href='https//:twitter.com/tesimune' target='_blank'><TiSocialTwitter/></a>
+        <a href='https//:linkedin.com/tesimune' target='_blank'><TiSocialLinkedin/></a>
+        <a href='https//:wa.com/tesimune' target='_blank'><SiWhatsapp/></a>
       </span>
-      <a href="#" >@2022 <strong>Teslim</strong> All Right Reserved.</a>
-      {/* <span className=''>
-        <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-      </span> */}   
+      <a href="#" >@2022 <strong>Teslim</strong> All Right Reserved.</a>  
     </footer>
   )
 }
