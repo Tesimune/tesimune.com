e9f6fae650a3d77e88c02928ee5317ba71698a95
Tesimune <Tesimune@gmail.com>
2023-03-25 23:28:56 +0100

    Header image updated

--- content/components/Header.js
+++ content/components/Header.js
@@ -1,13 +1,12 @@
 import Head from 'next/head'
-import Image from 'next/image'
 
 export default function Header() {
   return (
     <Head>
         <title>Tesimune</title>
-        <meta property="og:title" content="Tesimune Portfolio" />
+        <meta property="og:title" content="Tesimune" />
         <meta property="og:description" content="Teslime Tesimune Portfolio" />
-        <meta property="og:image" content="/profile.png" />
+        <meta property="og:image" content="/header.png" />
         <meta property="og:url" content="https://tesimune.vercel.app" />
         <link rel="icon" href="/profile.png" />
     </Head>
